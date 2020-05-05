f40d98a21b272eb8e7ab8c6fcd1a27bc9b5dca0f
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-05-05 14:37:33 +0200

    styled profile pic

--- src/components/Header.js
+++ src/components/Header.js
@@ -233,72 +233,7 @@ const ProfilePic = styled.div`
   width: 350px;
   height: 350px;
   border: solid 15px #000;
-
-  :hover {
-    -webkit-animation: wobble-hor-bottom 0.8s both;
-              animation: wobble-hor-bottom 0.8s both;
-
-    @-webkit-keyframes wobble-hor-bottom {
-      0%,
-      100% {
-        -webkit-transform: translateX(0%);
-                transform: translateX(0%);
-        -webkit-transform-origin: 50% 50%;
-                transform-origin: 50% 50%;
-      }
-      15% {
-        -webkit-transform: translateX(-30px) rotate(-6deg);
-                transform: translateX(-30px) rotate(-6deg);
-      }
-      30% {
-        -webkit-transform: translateX(15px) rotate(6deg);
-                transform: translateX(15px) rotate(6deg);
-      }
-      45% {
-        -webkit-transform: translateX(-15px) rotate(-3.6deg);
-                transform: translateX(-15px) rotate(-3.6deg);
-      }
-      60% {
-        -webkit-transform: translateX(9px) rotate(2.4deg);
-                transform: translateX(9px) rotate(2.4deg);
-      }
-      75% {
-        -webkit-transform: translateX(-6px) rotate(-1.2deg);
-                transform: translateX(-6px) rotate(-1.2deg);
-      }
-    }
-    @keyframes wobble-hor-bottom {
-      0%,
-      100% {
-        -webkit-transform: translateX(0%);
-                transform: translateX(0%);
-        -webkit-transform-origin: 50% 50%;
-                transform-origin: 50% 50%;
-      }
-      15% {
-        -webkit-transform: translateX(-30px) rotate(-6deg);
-                transform: translateX(-30px) rotate(-6deg);
-      }
-      30% {
-        -webkit-transform: translateX(15px) rotate(6deg);
-                transform: translateX(15px) rotate(6deg);
-      }
-      45% {
-        -webkit-transform: translateX(-15px) rotate(-3.6deg);
-                transform: translateX(-15px) rotate(-3.6deg);
-      }
-      60% {
-        -webkit-transform: translateX(9px) rotate(2.4deg);
-                transform: translateX(9px) rotate(2.4deg);
-      }
-      75% {
-        -webkit-transform: translateX(-6px) rotate(-1.2deg);
-                transform: translateX(-6px) rotate(-1.2deg);
-      }
-    }
-    
-    }
-  }
+  box-shadow: 30px 30px yellow;
 `;
 
 export const Header = () => {

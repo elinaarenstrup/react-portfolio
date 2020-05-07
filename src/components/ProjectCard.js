1843ccc099f8e7ccc3976999f5c2bb62bbf8223b
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-05-07 13:06:11 +0200

    Styled the project cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,22 +3,24 @@ import styled from "styled-components/macro";
 
 const CardContainer = styled.div`
   width: 300px;
-  height: 320px;
+  height: 370px;
   margin-right: 20px;
   margin-bottom: 20px;
+  border: 10px solid #000;
   background-color: #000;
 `;
 
 const ImgBox = styled.div`
   width: 100%;
   height: 200px;
-  margin: 0;
+  margin: 0 0 10px 0;
   background-color: yellow;
 `;
 
 const TextContainer = styled.div`
   margin: auto;
   display: flex;
+  padding: 0 20px;
   flex-direction: column;
   align-items: center;
   justify-items: center;
@@ -28,6 +30,15 @@ const Boldertext = styled.h2`
   font-family: "Bebas Neue";
   font-size: 42px;
   font-weight: bolder;
+  margin: 10px auto;
+  color: #fff;
+`;
+
+const Paragraph = styled.p`
+  display: block;
+  text-align: left;
+  font-family: "Roboto";
+  font-size: 14px;
   color: #fff;
 `;
 
@@ -37,6 +48,10 @@ export const ProjectCard = () => {
       <ImgBox />{" "}
       <TextContainer>
         <Boldertext>Title</Boldertext>
+        <Paragraph>
+          A music release page in pairs using a API, which shows new album and
+          single releases
+        </Paragraph>
       </TextContainer>
     </CardContainer>
   );
